b3c4cf94a0d965be63018593799e3213665eefcb
dgodolias <dgodolias@gmail.com>
2025-04-26 18:13:20 +0300

    master,worker,reducer seem to run fine ditributed(locally) in my ip. but the client wont yet

--- run_commands/distributed_package/master_reducer/master_8080.bat
+++ run_commands/distributed_package/master_reducer/master_8080.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Start the Master in distributed mode with reducer and workers
+REM Start the Master
 
 REM Get current IP address automatically
 FOR /F "tokens=2 delims=:" %%a IN ('ipconfig ^| findstr /R /C:"IPv4 Address"') DO (
@@ -12,23 +12,27 @@ REM Remove leading space from IP address
 SET CURRENT_IP=%CURRENT_IP:~1%
 echo Detected IP address: %CURRENT_IP%
 
+REM Default settings
 set REDUCER_HOST=%CURRENT_IP%
-set WORKER_ADDRS=%CURRENT_IP%:8081
+set REDUCER_PORT=8090
 set ROOT_DIR=..
 
-echo Starting Master in distributed mode
+REM Parse command-line arguments
+if not "%1"=="" set REDUCER_HOST=%1
+if not "%2"=="" set REDUCER_PORT=%2
+if not "%3"=="" set WORKER_ADDRS=%3
+
+REM Default worker addresses if not specified
+if "%WORKER_ADDRS%"=="" set WORKER_ADDRS=%CURRENT_IP%:8081,%CURRENT_IP%:8082,%CURRENT_IP%:8083,%CURRENT_IP%:8084,%CURRENT_IP%:8085
+
+echo Starting Master in distributed mode...
 echo - IP Address: %CURRENT_IP%
-echo - Reducer host: %REDUCER_HOST%
+echo - Connecting to Reducer at: %REDUCER_HOST%:%REDUCER_PORT%
 echo - Worker addresses: %WORKER_ADDRS%
-echo - Using classpath: %ROOT_DIR%\classes
 echo.
 
-REM Create main data directory if it doesn't exist
-if not exist "%ROOT_DIR%\data" mkdir "%ROOT_DIR%\data"
-
-if not "%1"=="" set REDUCER_HOST=%1
-if not "%2"=="" set WORKER_ADDRS=%2
-java -cp "%ROOT_DIR%\classes" com.example.backend.Master --distributed --reducer %REDUCER_HOST% --workers %WORKER_ADDRS%
+REM Start Master with all worker addresses
+java -cp "%ROOT_DIR%\classes" com.example.backend.Master --distributed --reducer %REDUCER_HOST%:%REDUCER_PORT% --workers %WORKER_ADDRS%
 
 if %ERRORLEVEL% NEQ 0 (
   echo Master failed to start! Error code: %ERRORLEVEL%
--- run_commands/distributed_package/master_reducer/reducer_8090.bat
+++ run_commands/distributed_package/master_reducer/reducer_8090.bat
@@ -12,17 +12,25 @@ REM Remove leading space from IP address
 SET CURRENT_IP=%CURRENT_IP:~1%
 echo Detected IP address: %CURRENT_IP%
 
-set WORKER_ADDR=%CURRENT_IP%:8081
+REM Default settings
+set REDUCER_PORT=8090
 set ROOT_DIR=..
 
-echo Starting Reducer
+REM Parse command-line arguments
+if not "%1"=="" set WORKER_ADDRS=%1
+if not "%2"=="" set REDUCER_PORT=%2
+
+REM Default worker addresses if not specified
+if "%WORKER_ADDRS%"=="" set WORKER_ADDRS=%CURRENT_IP%:8081,%CURRENT_IP%:8082,%CURRENT_IP%:8083,%CURRENT_IP%:8084,%CURRENT_IP%:8085
+
+echo Starting Reducer...
 echo - IP Address: %CURRENT_IP%
-echo - Connecting to worker: %WORKER_ADDR%
-echo - Using classpath: %ROOT_DIR%\classes
+echo - Port: %REDUCER_PORT%
+echo - Connected to Workers at: %WORKER_ADDRS%
 echo.
 
-if not "%1"=="" set WORKER_ADDR=%1
-java -cp "%ROOT_DIR%\classes" com.example.backend.Reducer %WORKER_ADDR%
+REM Start the Reducer
+java -Dreducer.port=%REDUCER_PORT% -cp "%ROOT_DIR%\classes" com.example.backend.Reducer %WORKER_ADDRS:,= %
 
 if %ERRORLEVEL% NEQ 0 (
   echo Reducer failed to start! Error code: %ERRORLEVEL%
